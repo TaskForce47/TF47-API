f7608cd0f300b918ed6b4622693dacba49f6dedb
Oliver <oliverkrue@googlemail.com>
2020-05-18 17:49:57 +0200

    add custom cors middleware

---
+++ TF47_Api/Middleware/CorsMiddleware.cs
@@ -0,0 +1,44 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace TF47_Api.Middleware
+{
+    public class CorsMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public CorsMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (context.Request.Headers["Host"] == "api.taskforce47.com")
+            {
+                context.Response.Headers.Add("Access-Control-Allow-Origin", context.Request.Headers["Host"]);
+            }
+
+            context.Response.Headers.Add("Access-Control-Allow-Credentials", "true");
+            context.Response.Headers.Add("Access-Control-Allow-Headers", "Content-Type, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Accept-Encoding, Content-Length, Content-MD5, Date, X-Api-Version, X-File-Name");
+            context.Response.Headers.Add("Access-Control-Allow-Methods", "POST,GET,PUT,PATCH,DELETE,OPTIONS");
+            if (context.Request.Method == "OPTIONS")
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.OK;
+                await context.Response.WriteAsync(string.Empty);
+            }
+
+            await _next(context);
+        }
+    }
+
+    public static class CorsMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseCorsMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<CorsMiddleware>();
+        }
+    }
+}
--- TF47_Api/Startup.cs
+++ TF47_Api/Startup.cs
@@ -79,6 +79,7 @@ namespace TF47_Api
 
             app.UseSwagger();
             app.UseSwaggerUI(options => { options.SwaggerEndpoint("/swagger/v1/swagger.json", "TF47 API V1"); });
+            app.UseCorsMiddleware();
             app.UseCors();
             //app.UseHttpsRedirection();
             app.UseCustomCookieAuthentication();
