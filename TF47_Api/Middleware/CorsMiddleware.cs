b88d1d81ad6f78f990d31d4cd4ae7cd385661f50
Oliver <oliverkrue@googlemail.com>
2020-05-24 20:31:56 +0200

    added development origin

--- TF47_Api/Middleware/CorsMiddleware.cs
+++ TF47_Api/Middleware/CorsMiddleware.cs
@@ -22,10 +22,13 @@ namespace TF47_Api.Middleware
         {
             var origin = context.Request.Headers["origin"];
             _logger.LogInformation(context.Request.Method);
-            _logger.LogInformation(origin);
-            
-            context.Response.Headers.Add("Access-Control-Allow-Origin", "https://gadget.taskforce47.com");
-            
+            _logger.LogInformation($"{origin} {context.Request.Path.Value}");
+
+            context.Response.Headers.Add("Access-Control-Allow-Origin",
+                context.Request.Headers["origin"] == "https://gadget.taskforce47.com:8080"
+                    ? "https://gadget.taskforce47.com:8080"
+                    : "https://gadget.taskforce47.com");
+
             context.Response.StatusCode = 200;
             context.Response.Headers.Add("Access-Control-Allow-Credentials", "true");
             context.Response.Headers.Add("Access-Control-Allow-Headers", "Content-Type, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Accept-Encoding, Content-Length, Content-MD5, Date, X-Api-Version, X-File-Name");
