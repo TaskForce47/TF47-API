9c67f4e3008e9956d6360451d085534962d64683
Oliver <oliverkrue@googlemail.com>
2020-06-26 20:50:46 +0200

    unify error responses

---
+++ TF47_Api/CustomStatusCodes/ServerError.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace TF47_Api.CustomStatusCodes
+{
+    public class ServerError : IActionResult
+    {
+        private string _errorMessage;
+
+        public ServerError(string errorMessage = "")
+        {
+            _errorMessage = errorMessage;
+        }
+
+
+        public async Task ExecuteResultAsync(ActionContext context)
+        {
+            var objectResult = new ObjectResult(new
+            {
+                ErrorMessage = _errorMessage
+            })
+            {
+                StatusCode = StatusCodes.Status500InternalServerError
+            };
+            await objectResult.ExecuteResultAsync(context);
+        }
+    }
+}
