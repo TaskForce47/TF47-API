9c79d625d2e0edd74aeb57b4acb749ea9fc9d828
Oliver <oliverkrue@googlemail.com>
2020-06-26 20:51:13 +0200

    added service to get forum user by httpcontext

---
+++ TF47_Api/Services/GadgetUserProviderService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using TF47_Api.Database;
+using TF47_Api.Models;
+
+namespace TF47_Api.Services
+{
+    public class GadgetUserProviderService
+    {
+        private readonly ILogger<GadgetUserProviderService> _logger;
+        private readonly Tf47DatabaseContext _database;
+
+        public GadgetUserProviderService(ILogger<GadgetUserProviderService> logger, Tf47DatabaseContext database)
+        {
+            _logger = logger;
+            _database = database;
+        }
+
+        public async Task<Tf47GadgetUser> GetGadgetUserFromHttpContext(HttpContext context)
+        {
+            var forumIdClaim = context.User.Claims.First(x => x.Type == CustomClaimTypes.ForumId).Value;
+            if (string.IsNullOrEmpty(forumIdClaim)) return null;
+
+            var currentUser = await _database.Tf47GadgetUser.FirstOrDefaultAsync(x => x.Id == uint.Parse(forumIdClaim));
+            if (currentUser == null)
+            {
+                _logger.LogWarning($"This should not be null, claim: {forumIdClaim}");
+                return null;
+            }
+
+            return currentUser;
+        }
+    }
+}
