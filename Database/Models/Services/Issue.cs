bc3bf1c3fb7a7f5f54caaaa6bab6e1d8c996e6c3
Oliver Krüppel <krueppel@cs.uni-bonn.de>
2021-03-16 12:59:29 +0100

    added issue tags

--- Database/Models/Services/Issue.cs
+++ Database/Models/Services/Issue.cs
@@ -11,12 +11,13 @@ namespace TF47_Backend.Database.Models.Services
         [MaxLength(200)]
         public string Title { get; set; }
         public bool IsClosed { get; set; }
-        public string[] Tags { get; set; }
         public User IssueCreator { get; set; }
         public DateTime TimeCreated { get; set; }
         public DateTime TimeLastUpdated { get; set; }
 
         public ICollection<IssueItem> IssueItems { get; set; }
+        public ICollection<IssueTag> IssueTags { get; set; }
+
         public IssueGroup IssueGroup { get; set; }
         public long IssueGroupId { get; set; }
     }
---
+++ Database/Models/Services/IssueTag.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace TF47_Backend.Database.Models.Services
+{
+    [Index(nameof(TagName), IsUnique = true)]
+    public class IssueTag
+    {
+        public long IssueTagId { get; set; }
+        public string TagName { get; set; }
+        public string Color { get; set; }
+
+        public ICollection<Issue> Issues { get; set; }
+    }
+}
