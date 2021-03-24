7c9e5dae4fa6d4fa7c3664983ddb00e0e4bc1f0d
Oliver Krüppel <krueppel@cs.uni-bonn.de>
2021-03-24 17:14:48 +0100

    grpc test example

---
+++ Grpc/TestService.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+
+namespace TF47_Backend.Grpc
+{
+    public class TestService : Greeter.GreeterBase
+    {
+        private readonly ILogger<TestService> _logger;
+
+        public TestService(ILogger<TestService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<HelloReply> SayHello(HelloRequest request,
+            ServerCallContext context)
+        {
+            _logger.LogInformation("Saying hello to {Name}", request.Name);
+            return Task.FromResult(new HelloReply
+            {
+                Message = "Hello " + request.Name
+            });
+        }
+    }   
+}
