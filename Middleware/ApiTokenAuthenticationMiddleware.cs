f66d1d4c6d67ab76467b62113bc369e527196b48
Oliver Krüppel <krueppel@cs.uni-bonn.de>
2021-03-24 17:14:38 +0100

    added new migrations

---
+++ Middleware/ApiTokenAuthenticationMiddleware.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TF47_Backend.Middleware
+{
+    public class ApiTokenAuthenticationMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ApiTokenAuthenticationMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            await _next(httpContext);
+        }
+    }
+}
