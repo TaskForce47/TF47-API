b6f543812d4b6231131a360238c47ec3c3488286
Oliver Krüppel <krueppel@cs.uni-bonn.de>
2021-03-24 17:15:38 +0100

    First AAR implementation

---
+++ Database/Models/GameServer/AAR/DataTypes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Threading.Tasks;
+
+namespace TF47_Backend.Database.Models.GameServer.AAR
+{
+    public interface IObject { };
+
+    public record Unit(string Name, string Side, string Group, string CurrentWeapon, bool IsIncapacitated, bool IsAi) : IObject;
+
+    public record Vehicle(string Name, string Type, float Damage) : IObject;
+
+    public record UpdatePos(float[] Pos, float[] Vector, IObject Object);
+
+    public record ProjectileFired(IObject Shooter, string Projectile, float[] Origin, float[] Vector);
+
+    public record Died(IObject Object);
+
+    public record Damaged(IObject Object, float OldValue, float NewValue);
+
+    public record Healed(IObject Healer, IObject Target);
+
+    //public record PositionData(string Name, string Side, int[] position)
+}
