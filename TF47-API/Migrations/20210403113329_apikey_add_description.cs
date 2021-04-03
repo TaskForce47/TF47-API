95ddb3defb555fe9b08fab57af122c7a62f98cbc
Oliver Krüppel <krueppel@cs.uni-bonn.de>
2021-04-03 13:34:15 +0200

    added database migrations for api key description

---
+++ TF47-API/Migrations/20210403113329_apikey_add_description.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TF47_API.Migrations
+{
+    public partial class apikey_add_description : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "description",
+                table: "ServiceApiKeys",
+                type: "character varying(200)",
+                maxLength: 200,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "description",
+                table: "ServiceApiKeys");
+        }
+    }
+}
