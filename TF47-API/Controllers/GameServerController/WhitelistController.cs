2181753b7c2173ccaecd62a96dac2c272d5afde8
Oliver Krüppel <krueppel@cs.uni-bonn.de>
2021-04-24 01:22:38 +0200

    added whitelisting controller

--- TF47-API/Controllers/GameServerController/WhitelistController.cs
+++ TF47-API/Controllers/GameServerController/WhitelistController.cs
@@ -1,7 +1,13 @@
 using System;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using TF47_API.Database;
+using TF47_API.Dto.Mappings;
+using TF47_API.Dto.RequestModels;
+using TF47_API.Dto.ResponseModels;
 using TF47_API.Services;
 
 namespace TF47_API.Controllers.GameServerController
@@ -21,8 +27,104 @@ namespace TF47_API.Controllers.GameServerController
             _database = database;
             _userProviderService = userProviderService;
         }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(WhitelistResponse), 200)]
+        public async Task<IActionResult> GetWhitelists()
+        {
+            var response = await _database.Whitelists
+                .AsNoTracking()
+                .ToListAsync();
+            return Ok(response.ToWhitelistResponseIEnumerable());
+        }
+
+        [HttpGet("user/{playerUid}")]
+        [ProducesResponseType(typeof(UserWhitelistingResponse), 200)]
+        public async Task<IActionResult> GetWhitelistingsUser(string playerUid)
+        {
+            var user = await _database.Players
+                .AsNoTracking()
+                .Include(x => x.PlayerWhitelistings)
+                .FirstOrDefaultAsync(x => x.PlayerUid == playerUid);
+
+            if (user == null)
+                return BadRequest("User Uid provided does not exist");
+            
+            return Ok(user.ToPlayerWhitelistingResponse());
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(UserWhitelistingResponse[]), 200)]
+        public async Task<IActionResult> GetAllWhitelistings()
+        {
+            var users = await _database.Players
+                .AsNoTracking()
+                .Include(x => x.PlayerWhitelistings)
+                .ToListAsync();
+            return Ok(users.ToPlayerWhitelistingResponseIEnumerable());
+        }
+
+        [HttpPost("userWhitelisting")]
+        public async Task<IActionResult> WhitelistUser([FromBody] CreateUserWhitelistingRequest request)
+        {
+            var whitelist = await _database.Whitelists.FirstOrDefaultAsync(x => x.WhitelistId == request.WhitelistId);
+            if (whitelist == null)
+                return BadRequest("Whitelist Id provided does not exist");
+
+            var player = await _database.Players.FirstOrDefaultAsync(x => x.PlayerUid == request.PlayerUid);
+            if (player == null)
+                return BadRequest("User Uid provided does not exist");
+
+            if (whitelist.Players.All(x => x.PlayerUid != player.PlayerUid))
+                whitelist.Players.Add(player);
+
+            await _database.SaveChangesAsync();
+            
+            return Ok();
+        }
         
-        
-        
+        [HttpPost("userWhitelistingBatch")]
+        public async Task<IActionResult> WhitelistUserBatch([FromBody] CreateUserWhitelistingRequest[] requests)
+        {
+            try
+            {
+                foreach (var request in requests)
+                {
+                    var whitelist =
+                        await _database.Whitelists.FirstOrDefaultAsync(x => x.WhitelistId == request.WhitelistId);
+                    if (whitelist == null)
+                        throw new Exception("Whitelist Id provided does not exist");
+
+                    var player = await _database.Players.FirstOrDefaultAsync(x => x.PlayerUid == request.PlayerUid);
+                    if (player == null)
+                        throw new Exception("User Uid provided does not exist");
+
+                    if (whitelist.Players.All(x => x.PlayerUid != player.PlayerUid))
+                        whitelist.Players.Add(player);
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Either a userUid or whitelist id does not exist!");
+            }
+            await _database.SaveChangesAsync();
+
+            return Ok();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> RemoveWhitelisting([FromBody] RemoveUserWhitelistingRequest request)
+        {
+            var whitelist = await _database.Whitelists.FirstOrDefaultAsync(x => x.WhitelistId == request.WhitelistId);
+            if (whitelist == null) return BadRequest("Whitelist Id provided does not exist");
+            var user = await _database.Players.FirstOrDefaultAsync(x => x.PlayerUid == request.PlayerUid);
+            if (user == null) return BadRequest("User Uid provided does not exist");
+
+            whitelist.Players.Remove(user);
+
+            await _database.SaveChangesAsync();
+
+            return Ok();
+        }
     }
 }
