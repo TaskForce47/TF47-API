928d6b1b3a5d4a0b1ca72af8d22b99c186bcbbc8
Oliver Krüppel <krueppel@cs.uni-bonn.de>
2021-04-25 17:24:54 +0200

    added db migrations for donations

---
+++ TF47-API/Migrations/20210425152439_add_donations.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace TF47_API.Migrations
+{
+    public partial class add_donations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ServiceDonations",
+                columns: table => new
+                {
+                    donation_id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    time_of_donation = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    user_id = table.Column<Guid>(type: "uuid", nullable: true),
+                    amount = table.Column<decimal>(type: "numeric", nullable: false),
+                    note = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_service_donations", x => x.donation_id);
+                    table.ForeignKey(
+                        name: "fk_service_donations_service_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "ServiceUsers",
+                        principalColumn: "user_id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_service_donations_user_id",
+                table: "ServiceDonations",
+                column: "user_id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ServiceDonations");
+        }
+    }
+}
