d1862c5cc67acc17313186d9a6857a16c011e125
Oliver Krüppel <krueppel@cs.uni-bonn.de>
2021-05-17 00:47:54 +0200

    added few request and update DTOs

---
+++ TF47-API/Dto/RequestModels/CreateGalleryRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace TF47_API.Dto.RequestModels
+{
+    public record CreateGalleryRequest(string Name, string Description);
+
+    public record UpdateGalleryRequest(string Name, string Description);
+
+    public record CreateGalleryImageRequest(string Name, string Description);
+}
